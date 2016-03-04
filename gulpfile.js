08d7f43e746dd85f4f7c0cbcb4c5ce9c246fd628
Evan Duncan <evan.e.duncan@gmail.com>
2016-03-03 23:22:51 -0600

    Create application structure

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
+var clean = require('gulp-clean');
+
+gulp.task('build', ['build:server', 'build:client']);
+
+gulp.task('build:server', () => {
+	var tsProject = ts.createProject(__dirname + '/server/tsconfig.json');
+	return gulp.src(path.join(__dirname + '/server/**/*.ts'))
+		.pipe(ts(tsProject))
+		.js
+		.pipe(gulp.dest(path.resolve('./server')));
+});
+
+gulp.task('build:client', ['sass'], () => {
+	var tsProject = ts.createProject(__dirname + '/public/tsconfig.json');
+	return gulp.src(path.join(__dirname + '/public/**/*.ts'))
+		.pipe(ts(tsProject))
+		.js
+		.pipe(gulp.dest(path.resolve('./public')));
+});
+
+gulp.task('sass', ['sass:cleanup']);
+
+gulp.task('sass:compile', () => {
+	return gulp.src([__dirname + '/public/**/*.sass', __dirname + '/public/**/*.scss'])
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest(__dirname + '/public/.tmp/css'));
+});
+
+gulp.task('sass:cleanup', ['concat:css'], () => {
+	return gulp.src(__dirname + '/public/.tmp/css')
+		.pipe(clean());
+});
+
+gulp.task('concat:css', ['sass:compile'], () => {
+	return gulp.src(__dirname + '/public/.tmp/css/**/*.css')
+		.pipe(concatCss('app.css'))
+		.pipe(gulp.dest(__dirname + '/public/css'));
+});
+
+gulp.task('start', ['build'], () => {
+	nodemon({
+		script: 'server/server.js',
+		ext: 'js html',
+		env: { 'NODE_ENV': 'development' }
+	});
+});
---
+++ server/server.ts
@@ -0,0 +1,21 @@
+import express = require('express');
+import morgan = require('morgan');
+import path = require('path');
+
+const port: number = process.env.PORT || 3000;
+const app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+	app.use(morgan('dev'));	
+} else {
+	app.use(morgan('common'));
+}
+
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
+app.use('/lib', express.static(path.join(__dirname, '..', 'node_modules')));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => console.log('listening to port 3000'));
